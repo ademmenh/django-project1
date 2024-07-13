75a445d3098f23a05b9d4342d7b5714f084a3f0d
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-07-13 12:11:45 +0100

    adding indexes

--- project1/app1/models.py
+++ project1/app1/models.py
@@ -8,10 +8,24 @@ class Client (models.Model):
     lname = models.CharField(max_length=15, null=False)
     bday = models.DateField(null=False)
 
+    class Meta:
+        indexes = [
+            models.Index(name="i_Client_id", fields=['id']),
+            models.Index(name="i_Client_fname", fields=['fname']),
+            models.Index(name="i_Client_lname", fields=['lname']),
+        ]
+
+
 class Post (models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, null=False)
     pday = models.DateField(null=False)
+
+    class Meta:
+        indexes = [
+            models.Index(name="i_Post_id", fields=["id"]),
+            models.Index(name="i_Post_name", fields=["name"]),
+        ]
 
 class ClientPost (models.Model):
     c_id = models.ForeignKey(Client, to_field="id", on_delete=models.CASCADE)
@@ -19,3 +33,6 @@ class ClientPost (models.Model):
 
     class Meta:
         unique_together = (("c_id", "p_id"),)
+        indexes = [
+            models.Index(name="i_ClientPost_FoerignKeys", fields=["c_id", "p_id"]),
+        ]
