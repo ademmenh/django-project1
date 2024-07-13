f570833cead73cf72a8c0f4ab5128822887c197e
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-07-13 04:11:38 +0100

    adding model
    adding Client class,
    adding Post class,
    adding ClienPost class for the relationship,

--- project1/app1/models.py
+++ project1/app1/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Client (models.Model):
+    id = models.IntegerField(primary_key=True)
+    fname = models.CharField(max_length=20, null=False)
+    lname = models.CharField(max_length=15, null=False)
+    bday = models.DateField(null=False)
+
+class Post (models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+    pday = models.DateField(null=False)
+
+class ClientPost (models.Model):
+    c_id = models.ForeignKey(Client, to_field="id", on_delete=models.CASCADE)
+    p_id = models.ForeignKey(Post, to_field="id", on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (("c_id", "p_id"),)
