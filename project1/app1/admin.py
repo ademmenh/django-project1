9b1d1b6692d7ecec8cf72f399d53dda91f4bfd73
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-07-21 16:00:07 +0100

    new test
    new structure for administrations

--- project1/app1/admin.py
+++ project1/app1/admin.py
@@ -1,5 +1,41 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Client)
-admin.site.register(models.User)
+
+
+
+
+class ClientAdmin (admin.ModelAdmin):
+    #list form
+    list_display = ("id", "fname", "lname", "bday", "iday", "salary")
+    list_filter = ("salary", )
+
+    #individual form
+    fieldsets = (("Personal info", {"fields" : (("fname", "lname"), "bday")}),
+                ("Job Info"      , {"fields" : ("iday", "salary")}))
+
+
+
+class ClientSalaryAdmin (admin.ModelAdmin):
+    #list form
+    list_display = ("salary", "rating")
+    list_filter = ("rating",)
+
+    #individual form
+    fieldsets = (("Salary", {"fields" : ("salary", )}),
+                 ("Rating", {"fields" : ("rating", )}))
+
+
+
+class UsersAdmin (admin.ModelAdmin):
+    list_display = ("id", "fname")
+    # list_filter = ()
+
+    ''' fields = ("Personal Informations", {"fields" : ("fname", )}, ("", {"fields" : ("", "", )}) , )'''
+
+
+
+
+admin.site.register(models.Client, ClientAdmin)
+admin.site.register(models.ClientSalary, ClientSalaryAdmin)
+admin.site.register(models.Users, UsersAdmin)
--- project1/app1/models.py
+++ project1/app1/models.py
@@ -30,25 +30,19 @@ class Client (models.Model):
 class ClientSalary(models.Model):
 
     CHOICES_RATING={
-        'A':'',
-        'B':'',
-        'C':'',
-        'D':'',
-        'E':'',
-        'F':'',
+        'A':'A',
+        'B':'B',
+        'C':'C',
+        'D':'D',
+        'E':'E',
+        'F':'F',
     }
 
     salary = models.IntegerField(primary_key=True)
     rating = models.CharField(max_length=1, choices=CHOICES_RATING, null=False)
 
-    def objects_create(self):
-        if self.rating not in self.CHOICES_RATING:
-            raise ValidationError(f"the rating {self.rating} is invalid")
-        else:
-            ClientSalary.objects.create(salary=self.salary, rating=self.rating)
 
-
-class User (models.Model):
+class Users (models.Model):
     id = models.BigAutoField(primary_key=True)
     fname = models.CharField(max_length=20, null=False)
 
