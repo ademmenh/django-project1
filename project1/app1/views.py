510299188adadf6fd75490b388262c49acb56d05
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-07-24 01:11:50 +0100

    new template
    adding new template using render method,

--- project1/app1/views.py
+++ project1/app1/views.py
@@ -8,22 +8,29 @@ from . import models
 
 
 def form(request):
+
     if request.method == "GET":
         form = forms.FormUser()
+        context = {"form":form, }
+        return render(request, 'form.html', context)
+
 
     elif request.method == "POST":
         form = forms.FormUser(request.POST)
-
 
         if form.is_valid():
             formSubmition = request.POST
             form.IsCorrect(formSubmition['fname'])
 
             models.Users.objects.create(fname=formSubmition['fname'])
-            return HttpResponse("done!")
-    
-    context = {"form":form, }
-    return render(request, 'form.html', context)
+
+            context = {'fname':request.POST['fname'], }
+            return render(request, 'done.html', context)
+
+    else:
+        context = {}
+        render(request, 'error.html', context)
+
 
 
 
